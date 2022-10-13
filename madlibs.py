ae80ccd9c54ab8215deeac7adbd6cc406b917f40
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2022-10-13 15:36:23 -0700

    first commit

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+adj = input("Adjective: ")
+verb = input("Verb: ")
+verb2 = input("Another verb: ")
+adverb = input("Adverb: ")
+famus_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb}. Stay hydrated and {verb2} {adverb} like you are a {famus_person}!"
+
+print('')
+print('')
+print(madlib)
